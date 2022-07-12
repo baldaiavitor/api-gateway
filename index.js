d7bf352e4e94c64f30cc4bb6bf353046abe80f4f
Diniz Vitor <diniz@baldaiasolutions.com>
2022-07-12 16:01:10 +0900

    handle multiple method requests

--- index.js
+++ index.js
@@ -3,24 +3,31 @@ const gateway = require('./gateway.config.json');
 const express = require('express');
 const app = express();
 
-// route
-app.get('/:endpoint', (req,res)=>{
-  // Sending This is the home page! in the page
+app.use('/:endpoint', (req,res)=>{
   var endpoint = req.params.endpoint;
-  if(!gateway.endpoints[endpoint]){
-    res.status(404).send(`/${endpoint} 404`);
+  var method = req.method.toString().toLowerCase();
+  var endpointInfo = gateway.endpoints[method][endpoint];
+  var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+
+  if(!endpointInfo){
+    res.status(404).send(`${req.method}::/${endpoint} 404 not found`);
     res.end();
     return;
   }
 
-  var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
   let response = {
+    method:req.method,
     fullUrl:fullUrl,
     endpointName:endpoint,
     gatewayConfig:gateway.endpoints[endpoint]
   }
   res.send(response);
   res.end();
+});
+
+app.get('/', (req,res)=>{
+    res.send("Welcome to DJIN Api gateway");
+    res.end();
 });
 
 // Listening to the port
